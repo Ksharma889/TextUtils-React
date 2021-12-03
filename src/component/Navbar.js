5dd02b73944e94fb61c2ad46314f0bb6528c60e7
Kushal <sharmaje360@gmail.com>
2021-12-03 17:28:19 +0530

    All function setup in TextUtils App

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     return (
         <nav className="navbar navbar-light navbar-expand-lg px-lg-5">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/"><i>{props.title}</i></a>
+                <a className="navbar-brand" href="/"><b><i>{props.title}</i></b></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
+    const handleUpperTextClick = ()=>{
+        const newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLowerTextClick = ()=>{
+        const newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleCapitalizeTextClick = ()=>{
+        const capitalizeText = (capText)=>{
+            return capText.toLowerCase().split(" ").map(word =>
+                word.charAt(0).toUpperCase() + word.slice(1)
+            ).join(" ");
+        }
+        const newText = capitalizeText(text);
+        setText(newText);
+    }
+
+    const handleClearTextClick = ()=>{
+        const newText = "";
+        setText(newText);
+    }
+
+    const handleCopyTextClick = ()=>{
+        navigator.clipboard.writeText(text);
+    }
+
+    const handleRemoveExtraSpaces = ()=>{
+        const newText = text.replace(/\s+/g,' ');
+        setText(newText);
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('');
     return (
         <div className="container my-5 py-4">
-            <h1 className="title pb-2">{props.textareaTitle}</h1>
-            <textarea placeholder="Enter text here" className="form-control" id="exampleFormControlTextarea" rows="8"></textarea>
+            <h1 className="title pb-2">{props.TextFormTitle}</h1>
+            <textarea placeholder="Enter text here" value={text} onChange={handleOnChange} className="form-control" id="exampleFormControlTextarea" rows="8"></textarea>
             <div className="buttons my-2">
-                <button type="button" className="btn btn-dark my-2 mx-1">Convert to Uppercase</button>
-                <button type="button" className="btn btn-dark my-2 mx-1">Convert to Lowercase</button>
-                <button type="button" className="btn btn-dark my-2 mx-1">Capitalize Text</button>
-                <button type="button" className="btn btn-dark my-2 mx-1">Clear Text</button>
-                <button type="button" className="btn btn-dark my-2 mx-1">Copy Text</button>
-                <button type="button" className="btn btn-dark my-2 mx-1">Remove Extra Spaces</button>
+                <button type="button" onClick={handleUpperTextClick} className="btn btn-dark my-2 mx-1">Convert to Uppercase</button>
+                <button type="button" onClick={handleLowerTextClick} className="btn btn-dark my-2 mx-1">Convert to Lowercase</button>
+                <button type="button" onClick={handleCapitalizeTextClick} className="btn btn-dark my-2 mx-1">Capitalize Text</button>
+                <button type="button" onClick={handleClearTextClick} className="btn btn-dark my-2 mx-1">Clear Text</button>
+                <button type="button" onClick={handleCopyTextClick} className="btn btn-dark my-2 mx-1">Copy Text</button>
+                <button type="button" onClick={handleRemoveExtraSpaces} className="btn btn-dark my-2 mx-1">Remove Extra Spaces</button>
             </div>
             <h3>Your Text Summary</h3>
-            <p>0 word and 0 character</p>
-            <p>0 Minutes to read</p>
+            <p>{((text.trim().split(" ")).filter((e) => e !== "")).length} word and {text.length} character</p>
+            <p>{0.008 * text.split(" ").length} Minutes to read</p>
             <h3>Preview</h3>
-            <p>Enter something in the textarea above to preview it here</p>
+            <p className='preview'>{text.length>0? text : 'Enter something in the textarea above to preview it here'}</p>
         </div>
     )
 }
