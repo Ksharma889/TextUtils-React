bdca78141810c073ace51d43e7c7fcaada931142
Kushal <sharmaje360@gmail.com>
2021-12-05 17:11:08 +0530

    create TextUtils about page

---
+++ src/component/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="about-page container py-lg-5">
+            <h1>About</h1>
+            <p className="bd-lead">Learn more about the team maintaining TextUtils, how and why the project started, and how to get involved.</p>
+            <div className="history mt-5">
+                <h3>Get involved</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, nesciunt unde non quo quisquam consectetur mollitia ipsam odit a incidunt sed praesentium repellendus temporibus aperiam soluta, totam, itaque cum dicta dolorum? Recusandae maxime voluptatum magni dignissimos perspiciatis rerum repellat cupiditate hic quis! Amet corrupti culpa excepturi atque debitis quae at?</p>
+                <p>Ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, nesciunt unde non quo quisquam consectetur mollitia ipsam odit a incidunt sed praesentium repellendus temporibus aperiam soluta, totam, itaque cum dicta dolorum? Recusandae maxime voluptatum magni dignissimos perspiciatis rerum repellat cupiditate hic quis! Amet corrupti culpa excepturi atque debitis quae at? lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, nesciunt unde non quo quisquam consectetur mollitia ipsam odit a incidunt sed praesentium repellendus temporibus aperiam soluta, totam, itaque cum dicta dolorum? Recusandae maxime voluptatum magni dignissimos perspiciatis rerum repellat cupiditate hic quis! Amet corrupti culpa excepturi atque debitis quae at?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam corporis ducimus eaque beatae voluptas ipsa quaerat modi libero magni? Laudantium!</p>
+            </div>
+        </div>
+    )
+}
